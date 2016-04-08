f6e873d45646f0cc6665cc3735365bee5b76b5da
backslashgreg <gregor@rydzynski.de>
2016-04-08 10:37:13 +0200

    Initial commit of the spring boot bare project setup

---
+++ src/main/java/de/hska/vs2/beschte/Babble/BabbleApplication.java
@@ -0,0 +1,12 @@
+package de.hska.vs2.beschte.Babble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabbleApplication.class, args);
+	}
+}
---
+++ src/test/java/de/hska/vs2/beschte/Babble/BabbleApplicationTests.java
@@ -0,0 +1,18 @@
+package de.hska.vs2.beschte.Babble;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BabbleApplication.class)
+@WebAppConfiguration
+public class BabbleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
